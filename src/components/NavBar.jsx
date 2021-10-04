032abc20d8e5267cb1d17155a3c944bd185a267c
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-04 17:43:26 -0400

    Create NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function menuButtonClick() {
+    setShowMenu(!showMenu);
+  }
+  function closeMenu() {
+    setShowMenu(false);
+  }
+  return (
+    <div>
+      <nav className="nav-bar">
+        <div className="menu-button" onClick={menuButtonClick}>
+          {showMenu ? (
+            <i class="fas fa-times"></i>
+          ) : (
+            <i class="fas fa-bars"></i>
+          )}
+        </div>
+        <a className="navbar-logo" href="#">
+          THOMAS NGUYEN
+        </a>
+        <div
+          className={
+            showMenu ? "nav-menu nav-menu-open" : "nav-menu nav-menu-close"
+          }
+        >
+          <ul className="nav-menu-content">
+            <li>
+              <a className="nav-menu-item" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="nav-menu-item" href="#">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a className="nav-menu-item" href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="nav-menu-item" href="#">
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default NavBar;
