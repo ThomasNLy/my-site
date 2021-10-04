5d4ea4c7aac31f57f8407f7b78d76b91318b6de6
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-04 17:42:02 -0400

    Create AboutSection.jsx

---
+++ src/AboutSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function AboutSection() {
+  return (
+    <section>
+      <h3>ABOUT</h3>
+      <p>
+        Hi! And welcome to my website. My name is Thomas and I am an aspiring
+        Web Developer/Front End Developer. I graduated with a degree in Digital
+        MEdia and some of my interests are UI/UX design, creative coding,
+        creating interactive media and watching Korean dramas.
+      </p>
+      <a>CV/Resume</a>
+    </section>
+  );
+}
+export default AboutSection;
