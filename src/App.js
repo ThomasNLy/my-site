9907afe41bc474a2d3fb1ff25b19fefc6c8d514e
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-04 17:42:30 -0400

    added various sections to webpage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 
 import "./App.css";
 // import Sketch from "react-p5";
+import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
+import SkillsSection from "./components/SkillsSection";
+import AboutSection from "./AboutSection";
 import ProjectSection from "./components/ProjectsSection";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="container">
-      <HeroSection />
-      <ProjectSection />
-      <Footer />
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <div className="container">
+        <HeroSection />
+        <SkillsSection />
+        <AboutSection />
+        <ProjectSection />
+        <Footer />
+      </div>
+    </React.Fragment>
   );
 }
 
