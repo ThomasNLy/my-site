fa3c34fa2a1952a5f5709d147ae805e633a8867b
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-04 17:43:56 -0400

    Create SkillsSection.jsx

---
+++ src/components/SkillsSection.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+function SkillsSection() {
+  return (
+    <section>
+      <h2>SKILLS</h2>
+      <h3>PROGRAMMING</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Java</li>
+      </ul>
+      <h2>FRAMEWORKS</h2>
+      <ul>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h2>DESIGN</h2>
+      <ul>
+        <li>Adobe XD</li>
+        <li>Photoshop</li>
+        <li>Illustrator</li>
+      </ul>
+    </section>
+  );
+}
+export default SkillsSection;
