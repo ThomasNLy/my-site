fcf11170d2b706b654b4fdd6b4d1ebd82cff12ec
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-11 16:08:18 -0400

    installed react router dom
    installed react router package using npm package installer in order to allow for multi page navigation.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
-// import Sketch from "react-p5";
+
 import NavBar from "./components/NavBar";
-import HeroSection from "./components/HeroSection";
-import SkillsSection from "./components/SkillsSection";
-import AboutSection from "./AboutSection";
-import ProjectSection from "./components/ProjectsSection";
 import Footer from "./components/Footer";
+
+import Home from "./components/pages/Home";
+import CovidStatsCan from "./components/pages/CovidStatsCan";
+import LoginSystem from "./components/pages/LoginSystem";
+import PhoStop from "./components/pages/PhoStop";
 
 function App() {
   return (
     <React.Fragment>
-      <NavBar />
-      <div className="container">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/covid-stats-can" component={CovidStatsCan} />
+          <Route path="/login-system" component={LoginSystem} />
+          <Route path="/phostop" component={PhoStop} />
+        </Switch>
+        <Footer />
+      </Router>
+      {/* <div className="container">
         <HeroSection />
         <SkillsSection />
         <AboutSection />
         <ProjectSection />
         <Footer />
-      </div>
+      </div> */}
     </React.Fragment>
   );
 }
