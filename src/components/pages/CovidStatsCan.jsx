d31f08cd95bc3a38687405e241c464aecdbba469
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-11 16:15:09 -0400

    Create CovidStatsCan.jsx

---
+++ src/components/pages/CovidStatsCan.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function CovidStatsCan() {
+  return (
+    <div className="container">
+      <h1>COVID STATS CAN</h1>
+      <p>Hello</p>
+    </div>
+  );
+}
+export default CovidStatsCan;
