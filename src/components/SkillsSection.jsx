ff4120624d38c15013483e0cd79dba3a1e98c65f
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-11 16:43:33 -0400

    added styling with css style sheet

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,26 +1,35 @@
 import React from "react";
+import "./SkillsSection.css";
 function SkillsSection() {
   return (
     <section>
-      <h2>SKILLS</h2>
-      <h3>PROGRAMMING</h3>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Java</li>
-      </ul>
-      <h2>FRAMEWORKS</h2>
-      <ul>
-        <li>React</li>
-        <li>Bootstrap</li>
-      </ul>
-      <h2>DESIGN</h2>
-      <ul>
-        <li>Adobe XD</li>
-        <li>Photoshop</li>
-        <li>Illustrator</li>
-      </ul>
+      <h2 className="section-header">SKILLS</h2>
+      <div className="skills">
+        <div className="skill-set">
+          <h3 className="">PROGRAMMING</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Java</li>
+          </ul>
+        </div>
+        <div className="skill-set">
+          <h3>FRAMEWORKS</h3>
+          <ul>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <div className="skill-set">
+          <h3>DESIGN</h3>
+          <ul>
+            <li>Adobe XD</li>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
