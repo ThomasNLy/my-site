fe7cf58f577c7591a5cc001fbe96c929b600fbd3
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-10-12 10:12:39 -0400

    wrapped project cards with a div
    wrapped project cards with a div so they can be styled and organized using css as a group.

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import styles from "./css-modules/Sections.module.css";
+import "./ProjectsSection.css";
 import covidStatThumbnail from "../images/covid_stats_thumbnail.png";
 import covidStatThumbnailMobile from "../images/covid_stats_thumbnail_mobile.png";
 import loginSystemThumbnail from "../images/login_system_thumbnail.png";
@@ -11,27 +12,29 @@ function ProjectSection() {
   return (
     <section>
       <h2 className={styles.sectionH2Header}>PROJECTS</h2>
-      <ProjectCard
-        imgSrc={covidStatThumbnailMobile}
-        imgSrcSet={`${covidStatThumbnailMobile} 400w, ${covidStatThumbnail} 800w`}
-        altText="thumbnail of COVID STATS CAN's landing page"
-        projectTitle="COVID STATS CAN"
-        link="/covid-stats-can"
-      />
-      <ProjectCard
-        imgSrc={loginSystemThumbnailMobile}
-        imgSrcSet={`${loginSystemThumbnailMobile} 400w, ${loginSystemThumbnail} 800w`}
-        altText="thumbnail of Login System's landing page"
-        projectTitle="LOGIN SYSTEM"
-        link="/login-system"
-      />
-      <ProjectCard
-        imgSrc={phostopThumbnail}
-        imgSrcSet={`${phostopThumbnailMobile} 400w, ${phostopThumbnail} 800w`}
-        altText="thumbnail of PhoStop's landing page"
-        projectTitle="PHOSTOP"
-        link="/phostop"
-      />
+      <div className="projects-container">
+        <ProjectCard
+          imgSrc={covidStatThumbnailMobile}
+          imgSrcSet={`${covidStatThumbnailMobile} 400w, ${covidStatThumbnail} 800w`}
+          altText="thumbnail of COVID STATS CAN's landing page"
+          projectTitle="COVID STATS CAN"
+          link="/covid-stats-can"
+        />
+        <ProjectCard
+          imgSrc={loginSystemThumbnailMobile}
+          imgSrcSet={`${loginSystemThumbnailMobile} 400w, ${loginSystemThumbnail} 800w`}
+          altText="thumbnail of Login System's landing page"
+          projectTitle="LOGIN SYSTEM"
+          link="/login-system"
+        />
+        <ProjectCard
+          imgSrc={phostopThumbnail}
+          imgSrcSet={`${phostopThumbnailMobile} 400w, ${phostopThumbnail} 800w`}
+          altText="thumbnail of PhoStop's landing page"
+          projectTitle="PHOSTOP"
+          link="/phostop"
+        />
+      </div>
     </section>
   );
 }
