218ddd7a95da6473201c9d17ff4a664ae75f4356
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2021-11-13 10:46:30 -0500

    Update LoginSystem.jsx

--- src/components/pages/LoginSystem.jsx
+++ src/components/pages/LoginSystem.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "../css-modules/ProjectArticle.module.css";
-import loginSystemThumbnailMobile from "../../images/login_system_thumbnail_mobile.png";
 import loginSystemThumbnail from "../../images/login_system_thumbnail.png";
 import loginSystemForgotpasswordPic from "../../images/login_system_forgotpassword.png";
+import loginSystemProfilePic from "../../images/login_system_profile.png";
 function LoginSystem() {
   return (
     <article>
@@ -10,17 +10,37 @@ function LoginSystem() {
       <figure>
         <img
           className={styles.articleImage}
-          src={loginSystemThumbnailMobile}
+          src={loginSystemThumbnail}
           alt="The login webpage's landing page"
-          srcSet={`${loginSystemThumbnailMobile} 400w ${loginSystemThumbnail} 800w`}
         />
         <figcaption className={styles.imageCaption}>Website</figcaption>
       </figure>
       <div className={styles.articleDescription}>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
-          voluptatem aperiam rerum fugit tempora delectus architecto asperiores
-          placeat ipsum eos.
+          The login page for a web service or app that allows users to sign up,
+          login in or delete their account.
+        </p>
+        <p>
+          My approach to this project was to create something that allowed me to
+          utilize my knowledge of CSS animation and JavaScript along with its
+          Object Oriented Programing (OOP) paradigm.
+        </p>
+        <p>
+          On the home page a user is able to login by entering their user name
+          and password and clicking the sign in button. They may also click on
+          the <b>Forgot Password</b> link which will redirect them to the Forgot
+          Password page in order to retrieve their password. In the event they
+          don’t have an account they may click on the <b>Join Us</b> link to
+          sign up for an account.
+        </p>
+        <p>
+          In order for a user to login in I had to have a back end database for
+          the webpage to interact with to verify that the username and password
+          entered was correct, however I wanted to focus on front end for this
+          project. My solution to this problem was to utilize the browser’s
+          storage to cache the information and act as my database. A default
+          account setup has also been setup with the username and password being
+          admin.
         </p>
         <p>Built with HTML, CSS and JavaScript.</p>
         <a
@@ -43,12 +63,16 @@ function LoginSystem() {
         src={loginSystemForgotpasswordPic}
         alt=""
       />
+      <img className={styles.articleImage} src={loginSystemProfilePic} alt="" />
       <div className={styles.articleDescription}>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cum
-          obcaecati quibusdam architecto laudantium, doloribus veniam similique
-          asperiores repellat molestiae nemo vel saepe dolores dicta vero
-          nostrum, laboriosam temporibus et?
+          One of the most important things I wanted to focus on with this
+          project was the UI and UX. I wanted to ensure that feedback was given
+          whenever a user interacted with the website. Such as implementing a
+          banner to scroll down on the forgot password page to let them know an
+          email has been sent to reset their password. Along with that was
+          having a modal pop up if they chose to delete their account to focus
+          their attention on the decision they were about to make.{" "}
         </p>
       </div>
     </article>
