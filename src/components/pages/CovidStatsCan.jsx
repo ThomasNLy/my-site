a5fefbc5ff4a5e8ea884fdf1be65746ea22c9320
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2021-11-15 15:13:35 -0500

    added better place holder content
    styled the header and added the proper images for the page

--- src/components/pages/CovidStatsCan.jsx
+++ src/components/pages/CovidStatsCan.jsx
@@ -1,10 +1,54 @@
 import React from "react";
+import styles from "../css-modules/ProjectArticle.module.css";
+import covidStatsThumbnail from "../../images/covid_stats_thumbnail.png";
+import covidStatsStatusBar from "../../images/covid_stats_statusBar.png";
 function CovidStatsCan() {
   return (
-    <div>
-      <h1>COVID STATS CAN</h1>
-      <p>Hello</p>
-    </div>
+    <article>
+      <h1 className={styles.projectTitle}>COVID STATS CAN</h1>
+      <figure>
+        <img
+          className={styles.articleImage}
+          src={covidStatsThumbnail}
+          alt="The Covid Stats Can webpage's landing page"
+        />
+        <figcaption className={styles.imageCaption}>Website</figcaption>
+      </figure>
+      <div className={styles.articleDescription}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
+          molestias placeat doloremque a quae rerum minus vitae illo nemo
+          doloribus?
+        </p>
+        <p>Built with HTML, CSS and JavaScript.</p>
+        <a
+          href="https://github.com/ThomasNLy/project_3_covid_stat_website"
+          rel="noreferrer"
+          className={styles.projectLink}
+        >
+          Source Code
+        </a>
+        <a
+          href="https://thomasnly.github.io/project_3_covid_stat_website/"
+          rel="noreferrer"
+          className={styles.projectLink}
+        >
+          Go to website
+        </a>
+      </div>
+      <img
+        className={styles.articleImage}
+        src={covidStatsStatusBar}
+        alt="A figure displaying the various status bars and their different meanings: number of cases, recovered and mortality rate"
+      />
+      <div className={styles.articleDescription}>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
+          officia ipsam maiores pariatur ad in consequatur minima libero placeat
+          aperiam.
+        </p>
+      </div>
+    </article>
   );
 }
 export default CovidStatsCan;
