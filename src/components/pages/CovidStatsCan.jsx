4e4aac6daea9ca1f061090de1d89f9d9290981c0
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2021-11-16 15:31:21 -0500

    replaced placeholder text with actual content

--- src/components/pages/CovidStatsCan.jsx
+++ src/components/pages/CovidStatsCan.jsx
@@ -16,11 +16,17 @@ function CovidStatsCan() {
       </figure>
       <div className={styles.articleDescription}>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
-          molestias placeat doloremque a quae rerum minus vitae illo nemo
-          doloribus?
+          The purpose of this project was for me to learn how to create a react
+          web app along with integrating web APIs into websites. The idea came
+          to me as I kept hearing on the news and multiple media outlets how bad
+          the COVID-19 pandemic was getting on a daily basis, but I wasn’t able
+          to find any concrete statistics in relation to the virus. Examples of
+          this being the total amount of active cases/percentage of the
+          population infected, the recovery rate for infected individuals and
+          mortality rate. With this project I hope to accomplish just that
+          through data visualization.
         </p>
-        <p>Built with HTML, CSS and JavaScript.</p>
+        <p>Built with ReactJS, CSS and Open Covid API.</p>
         <a
           href="https://github.com/ThomasNLy/project_3_covid_stat_website"
           rel="noreferrer"
@@ -43,9 +49,16 @@ function CovidStatsCan() {
       />
       <div className={styles.articleDescription}>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
-          officia ipsam maiores pariatur ad in consequatur minima libero placeat
-          aperiam.
+          I wanted to present the information in a way that would be easily
+          readable for the user rather than just showing pure stats in the form
+          of numbers and percentages. Originally the thought of having it purely
+          be numbers would suffice but I wanted a point of reference for what
+          the numbers were supposed to relate too. By using bars that fill up
+          based on the number of cases, recovery rate and mortality rate in
+          relation to each provinces’ population it gives a better perspective
+          on the severity of the situation through a visual basis. Along with
+          that is their respective colour coordination to indicate what each bar
+          represents.
         </p>
       </div>
     </article>
